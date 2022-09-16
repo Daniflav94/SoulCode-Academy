945e9ae32cccf73b89c19fbe476af2cf501e64cb
Daniflav94 <danyelly_mell@hotmail.com>
2022-09-16 17:13:58 -0300

    exercicio 1

---
+++ javascript/exercícios/16-09/ex1.js
@@ -0,0 +1,6 @@
+const base = Number(prompt("Digite o valor da base do retângulo: "))
+const altura = Number(prompt("Digite o valor da altura do retângulo: "))
+
+const area = base * altura
+
+alert("Área do retângulo: " + area)
